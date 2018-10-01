6bf650c0a38284f614fb5d47240d0eba2ffe96e6
Ruiru11 <erastusruiru11gmail.com>
2018-10-02 00:45:52 +0300

    fiexd cursor in connection object

--- app/models/Database.py
+++ app/models/Database.py
@@ -7,12 +7,12 @@ class Database_connection():
     def __init__(self):
         try:
             self.connection = psycopg2.connect(
-                "dbname='ruiru' user='ruiru' password='' host='localhost' port='5432'"
+                "dbname='ruiru' user='ruiru' password='ruiru' host='localhost' port='5432'"
             )
             self.connection.autocommit = True
             self.cursor = self.connection.cursor()
         except (Exception, psycopg2.DatabaseError) as error:
-            print("CANNOT CONNECT TO DATABASE INVALID DATABASE URL")
+            print("CANNOT CONNECT TO DATABASE INVALID DATABASE URL",error)
 
     def create_tables(self):
         for command in commands:
