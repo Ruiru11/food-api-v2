23624b5d7eafeb2045c309196baa745b2ad93384
Ruiru11 <erastusruiru11gmail.com>
2018-10-02 23:36:46 +0300

    [ft:#160771140] secure endpoints

--- app/views/orders.py
+++ app/views/orders.py
@@ -1,14 +1,18 @@
 from flask import Blueprint
 from flask_restful import reqparse
 from app.controllers.orders import Orders
+from app.controllers.users import Users
 
 order_instance = Orders()
+usr = Users()
+
 
 mod_orders = Blueprint('orders', __name__, url_prefix='/api/v2')
 
 
 @mod_orders.route('/orders', methods=['POST'])
-def create_order():
+@usr.logged_in
+def create_order(res=None, user_id=None):
     parser = reqparse.RequestParser()
     parser.add_argument('user_id', type=int, location="json")
     parser.add_argument('item', type=str, location="json")
@@ -21,19 +25,24 @@ def create_order():
 
 
 @mod_orders.route('/orders', methods=['GET'])
-def get_orders():
+@usr.logged_in
+def get_orders(res=None, user_id=None):
     return order_instance.get_orders()
 
 
 @mod_orders.route('/orders/<int:id>', methods=['GET'])
-def get_order(id):
+@usr.logged_in
+def get_order(id, res=None, user_id=None):
     return order_instance.get_order(id)
 
 
 @mod_orders.route('/orders/<int:id>', methods=['PUT'])
-def update_order(id):
+@usr.logged_in
+def update_order(id, res=None, user_id=None):
     return order_instance.update_order(id)
-    
+
+
 @mod_orders.route('/orders/<int:id>', methods=['DELETE'])
-def delete_order(id):
+@usr.logged_in
+def delete_order(id, res=None, user_id=None):
     return order_instance.delete_order(id)
