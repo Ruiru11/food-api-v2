250fc837a90e84130f6df85895d41bc8ab714904
Ruiru11 <erastusruiru11gmail.com>
2018-10-02 23:27:56 +0300

    [ft:#160771140] secure endpoints

--- app/controllers/menus.py
+++ app/controllers/menus.py
@@ -3,22 +3,23 @@ from app.models.Database import Database_connection
 import psycopg2
 import uuid
 
+
 class Menus(object):
     def __init__(self):
         self.connection = Database_connection()
 
-    def create_menu(self,data):
+    def create_menu(self, data):
         try:
             meal_id = str(uuid.uuid4())
             self.connection.cursor.execute("""INSERT INTO meals(meal_id, name, description)
             VALUES(%s, %s, %s);""",
-                                (meal_id, data['name'], data['description']))
+                                           (meal_id, data['name'], data['description']))
             print("INSERTING DATA into MEALS")
             response_object = {
-                "status":"Item Added to Menu "
+                "status": "Item Added to Menu "
             }
             return(make_response(jsonify(response_object)))
-            
+
         except (Exception, psycopg2.DatabaseError) as error:
             print("ERROR inserting into meals", error)
 
@@ -28,10 +29,10 @@ class Menus(object):
         )
         news = self.connection.cursor.fetchall()
         return(jsonify(news))
-    
+
     def get_menus(self, id):
         self.connection.cursor.execute(
-            "SELECT * FROM  meals WHERE meal_id=%s",[id]
+            "SELECT * FROM  meals WHERE meal_id=%s", [id]
         )
         news = self.connection.cursor.fetchone()
         return(make_response(jsonify(news)))
