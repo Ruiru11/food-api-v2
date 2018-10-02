e115537ec5f041cea2e7ff0b4013ab51e9327d89
Ruiru11 <erastusruiru11gmail.com>
2018-10-02 23:36:10 +0300

    [ft:#160771140] secure endpoints

--- app/views/menus.py
+++ app/views/menus.py
@@ -8,9 +8,10 @@ mod_menus = Blueprint('menus', __name__, url_prefix='/api/v2')
 mnu = Menus()
 usr = Users()
 
+
 @mod_menus.route('/menus', methods=['POST'])
 @usr.logged_in
-def create_menu(res=None,user_id=None):
+def create_menu(res=None, user_id=None):
     parser = reqparse.RequestParser()
     parser.add_argument('name', type=str, location='json')
     parser.add_argument('description', type=str, location='json')
@@ -18,9 +19,10 @@ def create_menu(res=None,user_id=None):
 
     return mnu.create_menu(data)
 
+
 @mod_menus.route('/menus', methods=['GET'])
 @usr.logged_in
-def get_menu(res=None,user_id=None):
+def get_menu(res=None, user_id=None):
     return mnu.get_menu()
 
 
