a63219798af0a389b6a9c30ec2f243327c2f1690
Ruiru11 <erastusruiru11gmail.com>
2018-10-04 01:16:16 +0300

    [ft:160771018] As an admin  i should be able to update an order

--- app/views/users.py
+++ app/views/users.py
@@ -6,23 +6,32 @@ mod_users = Blueprint('users', __name__, url_prefix='/api/v2')
 
 usr = Users()
 
+
 @mod_users.route('/signup', methods=['POST'])
 def signup():
     parser = reqparse.RequestParser()
-    parser.add_argument('email',type=str, required=True, help="Pease provide an email", location="json")
-    parser.add_argument('address',type=str, required=True, help="Pease provide an address", location="json")
-    parser.add_argument('password',type=str,required=True, help="Pease provide a password",location="json")
-    parser.add_argument('username',type=str, required=True, help="Pease provide username",location="json")
+    parser.add_argument('email', type=str, required=True,
+                        help="Pease provide an email", location="json")
+    parser.add_argument('address', type=str, required=True,
+                        help="Pease provide an address", location="json")
+    parser.add_argument('password', type=str, required=True,
+                        help="Pease provide a password", location="json")
+    parser.add_argument('username', type=str, required=True,
+                        help="Pease provide username", location="json")
     data = parser.parse_args()
     return usr.create_user(data)
+
 
 @mod_users.route('/signin', methods=['POST'])
 def signin():
     parser = reqparse.RequestParser()
-    parser.add_argument('email',type=str, required=True, help="Pease provide an email", location="json")
-    parser.add_argument('password',type=str, required=True, help="Pease provide an password", location="json")
+    parser.add_argument('email', type=str, required=True,
+                        help="Pease provide an email", location="json")
+    parser.add_argument('password', type=str, required=True,
+                        help="Pease provide an password", location="json")
     data = parser.parse_args()
     return usr.sign_in(data)
+
 
 @mod_users.route('/users', methods=['GET'])
 @usr.logged_in
