cd1703674cbc3b2284a2b220d897876e2c8127a9
Ruiru11 <erastusruiru11gmail.com>
2018-10-04 11:37:11 +0300

    [ch:#160981896] test for more functionalities of the menus method

--- tests/test_menus.py
+++ tests/test_menus.py
@@ -10,30 +10,63 @@ class MenusTestCase(unittest.TestCase):
         self.app = create_app(config_name="test")
         self.client = self.app.test_client
         self.menu_data = {
-            "name": "ugali",
-            "description": "sweet"
+            "name": "waru",
+            "description": "sweet",
+            "price": "500"
         }
 
         self.user_data = {
             "email": "shs@mail.com",
             "password": "shssss",
             "address": "utawala",
-            "username": "ruiru"
+            "username": "ruiru",
+            "role": "admin"
         }
         self.login_data = {
             "email": "shs@mail.com",
             "password": "shssss"
         }
+        self.admin_data = {
+            "email": "admin@mail.com",
+            "password": "adminpassword"
+        }
 
         with self.app.app_context():
             db.create_tables()
 
-    def test_create_new_user(self):
+    def test_admin_login(self):
         res = self.client().post(
-            "api/v2/signup",
-            data=json.dumps(self.user_data),
+            "api/v2/signin",
+            data=json.dumps(self.admin_data),
             headers={"content-type": "application/json"}
+        )
+        self.assertEqual(res.status_code, 200)
 
+    def get_admin_token(self):
+        res = self.client().post(
+            "api/v2/signin",
+            data=json.dumps(self.admin_data),
+            headers={"content-type": "application/json"}
+        )
+        response = json.loads(res.data.decode('utf-8'))['token']
+        return response
+
+    def test_admin_create_menu(self):
+        token = self.get_admin_token()
+        res = self.client().post(
+            "api/v2/menus",
+            data=json.dumps(self.menu_data),
+            headers={"content-type": "application/json",
+                     "Authorization": token
+                     }
+        )
+        self.assertEqual(res.status_code, 200)
+
+    def test_user_signin(self):
+        res = self.client().post(
+            "api/v2/signin",
+            data=json.dumps(self.login_data),
+            headers={"content-type": "application/json"}
         )
         self.assertEqual(res.status_code, 200)
 
@@ -46,30 +79,23 @@ class MenusTestCase(unittest.TestCase):
         response = json.loads(res.data.decode('utf-8'))['token']
         return response
 
-    def test_create_menu(self):
-        # tests if an menu is created succsefully
+    def test_normal_user_creating_menu(self):
         token = self.get_user_token()
         res = self.client().post(
-            "/api/v2/menus",
+            "api/v2/menus",
             data=json.dumps(self.menu_data),
-            headers={
-                "content-type": "application/json",
-                "Authorization": token
-
-            }
-        )
-        self.assertEqual(res.status_code, 200)
-
-    def test_create_menu_without_token(self):
-        # tests authorization needed
-        res = self.client().post(
-            "/api/v2/menus",
-            data=json.dumps(self.menu_data),
-            headers={
-                "content-type": "application/json"
-            }
+            headers={"content-type": "application/js",
+                     "Authorization": token
+                     }
         )
         self.assertEqual(res.status_code, 401)
+
+    def test_anyone_can_view_menus(self):
+        res = self.client().get(
+            "api/v2/menus",
+            headers={"content-type": "application/json"}
+        )
+        self.assertEqual(res.status_code, 200)
 
 
 if __name__ == '__main__':
